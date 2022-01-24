e0177eb022f68352960b79c34917047bc5d2993a
Matthew Horwood <matt@horwood.biz>
2022-01-24 21:46:50 +0000

    Updates

---
+++ precache-manifest.d8367085994223d0f52daf58c550b7c3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d59bc5474ca62cb7cd3d92134ac9443",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2383bf525eab65d604f7",
+    "url": "/static/css/main.25a26d22.chunk.css"
+  },
+  {
+    "revision": "641252f7f278fe139c39",
+    "url": "/static/js/2.92e91fb2.chunk.js"
+  },
+  {
+    "revision": "40046c021c0025c71b5ec67323b92a85",
+    "url": "/static/js/2.92e91fb2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2383bf525eab65d604f7",
+    "url": "/static/js/main.110c7ec0.chunk.js"
+  },
+  {
+    "revision": "67710c546e8f728274d6",
+    "url": "/static/js/runtime-main.572c25bd.js"
+  }
+]);
